560e26ae80b9f1c11bbba2c617c7b6f279fc17e4
dawid <budzynski.dawid@gmail.com>
2018-10-20 18:26:42 +0200

    descriptions added

--- restaurants/dictionary.py
+++ restaurants/dictionary.py
@@ -4,25 +4,60 @@ DESCRIPTION = {
               'notable of which are kluski as well as cereals such as kasha. Generally speaking, Polish cuisine '
               'is hearty and heavy in its use of butter, cream and eggs. The traditional dishes are often demanding '
               'in preparation.',
-    'CAFE': 'Description will come soon.',
-    'ITALIAN': 'Description will come soon.',
-    'PIZZA': 'Description will come soon.',
-    'DESSERTS': 'Description will come soon.',
-    'EUROPEAN': 'Description will come soon.',
-    'FAST_FOOD': 'Description will come soon.',
+    'CAFE': 'A coffeehouse, coffee shop, or café is an establishment that primarily serves coffee, related coffee '
+            'drinks (latte, cappuccino, espresso), and – depending on country – other drinks including alcoholic. '
+            'Some coffeehouses may serve cold drinks such as iced coffee and iced tea; in continental Europe, '
+            'cafés serve alcoholic drinks. A coffeehouse may also serve some type of food, such as light snacks, '
+            'muffins or pastries. Coffeehouses range from owner-operated small businesses to large '
+            'multinational corporations.',
+    'ITALIAN': 'Italian cuisine is generally characterized by its simplicity, with many dishes having only two to '
+               'four main ingredients. Italian cooks rely chiefly on the quality of the ingredients rather than '
+               'on elaborate preparation. Ingredients and dishes vary by region. Many dishes that were once '
+               'regional have proliferated with variations throughout the country.',
+    'PIZZA': 'Pizza is a savoury dish of Italian origin, consisting of a usually round, flattened base of '
+             'leavened wheat-based dough topped with tomatoes, cheese, and various other ingredients (anchovies, '
+             'olives, meat, etc.) baked at a high temperature, traditionally in a wood-fired oven. In formal '
+             'settings, like a restaurant, pizza is eaten with knife and fork, but in casual settings it is cut '
+             'into wedges to be eaten while held in the hand. Small pizzas are sometimes called pizzettas.',
+    'DESSERTS': 'Dessert is a course that concludes a meal; oftentimes an evening meal. The course usually '
+                'consists of sweet foods, such as confections dishes or fruit, and possibly a beverage such as '
+                'dessert wine or liqueur, but may include coffee, cheeses, nuts, or other savory items. '
+                'In some parts of the world, such as much of central and western Africa, and most parts of China, '
+                'there is no tradition of a dessert course to conclude a meal.',
+    'EUROPEAN': 'European cuisine, or alternatively Western cuisine, is a generalised term collectively referring '
+                'to the cuisines of Europe and other Western countries. Steak and cutlet in particular are '
+                'common dishes across the West. Western cuisines also put substantial '
+                'emphasis on grape wine and on sauces as condiments, seasonings, or accompaniments. Many '
+                'dairy products are utilised in the cooking process, except in nouvelle cuisine. ',
+    'FAST_FOOD': "The fastest form of 'fast food' consists of pre-cooked meals kept in readiness for a "
+                 "customer's arrival, "
+                 "with waiting time reduced to mere seconds. Other fast food outlets, primarily the hamburger "
+                 "outlets use mass-produced pre-prepared ingredients but take great pains to point out to "
+                 "the customer that the 'meat and potatoes' are always cooked fresh and assembled 'to order'.",
     'BRITISH': 'Description will come soon.',
     'OTHERS': 'Description will come soon.',
     'INDIAN': 'Description will come soon.',
     'INTERNATIONAL': 'Description will come soon.',
-    'FINGER_FOOD': 'Description will come soon.',
+    'FINGER_FOOD': 'Finger food is food meant to be eaten directly using the hands, in contrast to food eaten with '
+                   'a knife and fork, spoon, chopsticks, or other utensils. In some cultures, food is almost '
+                   'always eaten with the hands; for example, Ethiopian cuisine is eaten by rolling various '
+                   'dishes up in injera bread. Foods considered street foods are frequently, though not '
+                   'exclusively, finger foods.',
     'KEBAB': 'Description will come soon.',
     'DRINKS_ONLY': 'Description will come soon.',
     'ISRAELI': 'Description will come soon.',
     'MEDITERRANEAN': 'Description will come soon.',
-    'BURGER': 'Description will come soon.',
+    'BURGER': 'A hamburger, beefburger or burger is a sandwich consisting of one or more cooked patties of ground '
+              'meat, usually beef, placed inside a sliced bread roll or bun. The patty may be pan fried, '
+              'grilled, or flame broiled. Hamburgers are often served with cheese, lettuce, tomato, bacon, '
+              'onion, pickles, or chiles; condiments such as mustard, mayonnaise, ketchup, relish, or '
+              '"special sauce"; and are frequently placed on sesame seed buns.',
     'AMERICAN': 'Description will come soon.',
     'JAPANESE': 'Description will come soon.',
-    'ASIAN': 'Description will come soon.',
+    'ASIAN': 'Asian cuisine includes several major regional cuisines: East Asian, Southeast Asian, South Asian, '
+             'Central Asian, and Middle Eastern/Western Asian. Ingredients common to many cultures in the '
+             'East and Southeast regions of the continent include rice, ginger, garlic, sesame seeds, chilies, '
+             'dried onions, soy, and tofu. Stir frying, steaming, and deep frying are common cooking methods.',
     'THAI': 'Description will come soon.',
     'SEAFOOD': 'Description will come soon.',
     'STEAK': 'Description will come soon.',
